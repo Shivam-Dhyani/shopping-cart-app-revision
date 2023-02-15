07e8a236f9d20d4db31f7be7201a3ebd83f44cc5
Shivam Dhyani <shivamdhiyani15@gmail.com>
2023-02-15 07:06:40 +0530

    Cart Page Partially Done

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import { ListGroup } from "react-bootstrap";
+import { CartState } from "../../contexts/Context";
 
 const Cart = () => {
-  return <div>Cart</div>;
+  const {
+    state: { cart },
+    dispatch,
+  } = CartState();
+
+  return (
+    <div className="home">
+      <div className="productContainer">
+        <ListGroup>
+          {cart.map((prod) => (
+            <span>{prod.name}</span>
+          ))}
+        </ListGroup>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
